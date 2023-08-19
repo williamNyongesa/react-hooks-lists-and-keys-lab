3775fb2e489ad5b1b07b21ddc1ab1acc401e04f6
williamNyongesa <marakawilliam@gmail.com>
2023-08-19 09:53:50 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,11 @@ function About({ bio, links }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {bio && bio.length > 1 ? <p>{bio}</p> : null}
+      {/* {bio && bio.length > 1 ? <p>{bio}</p> : null} */}
+
+
+
+      {bio && bio.length > 1? <p>{bio}</p>:null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={links.github} linkedin={links.linkedin} />
     </div>
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,35 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  console.log("my projects are: "+projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+
+
+
+      {projects.map((project,index) => (
+        <ProjectItem
+        key={index}
+          name={project.name}
+          about={project.about}
+          technologies={project.technologies}
+        />
+
+
+
+
+        
+      ))}
+
+
+
+
+
+      </div>
     </div>
   );
-}
+};
 
 export default ProjectList;
